4da393795db9af77258673c1c5311ab046ca44da
Daniel <damm172004@gmail.com>
2024-12-14 15:24:27 -0400

    first commit

---
+++ MyFirstVueApp/src/components/MyApp01.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+    <div>Mi primera app en <span>Vue.Js</span></div>
+</template>
+
+<style scoped>
+    div{
+        font-size: 30px;
+        text-align: center;
+        color: #2c3e50;
+    }
+    span{
+        color: #42b983;
+        font-weight: 700;
+    }
+</style>
